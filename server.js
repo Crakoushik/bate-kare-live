b9b7c7322a9516da840fccacc1fc8e32b587cc41
KOUSHIK MANDAL <51336050+KOUSHIK1997M@users.noreply.github.com>
2022-12-30 00:16:30 +0530

    commit

--- server.js
+++ server.js
@@ -10,17 +10,23 @@ http.listen(process.env.PORT || 3000, () => {
 
 //vercel 
 
-if (process.env.NODE_ENV == 'production') {
-    app.use(express.static(__dirname + '/public'));
-    app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/index.html');
-    })
-} else {
-    app.use(express.static(__dirname + '/public'));
-    app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/index.html');
-    })
-}
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+// if (process.env.NODE_ENV == 'production') {
+//     app.use(express.static(__dirname + '/public'));
+//     app.get('/', (req, res) => {
+//         res.sendFile(__dirname + '/index.html');
+//     })
+// } else {
+//     app.use(express.static(__dirname + '/public'));
+//     app.get('/', (req, res) => {
+//         res.sendFile(__dirname + '/index.html');
+//     })
+// }
 
 //-socket setUp in server.js-//
 const io = require('socket.io')(http);
