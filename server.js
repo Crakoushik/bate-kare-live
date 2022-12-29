a329a8605142a44061ca89c843a7ccc1a89ec435
KOUSHIK MANDAL <51336050+KOUSHIK1997M@users.noreply.github.com>
2022-12-30 00:58:54 +0530

    commit

--- server.js
+++ server.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 
-http.listen(process.env.PORT || 3000, () => {
-    console.log("Listening on port " + (process.env.PORT || 3000));
-})
-
 
 
 //vercel 
@@ -14,27 +10,12 @@ if (process.env.NODE_ENV == 'production') {
     app.get('/', (req, res) => {
         res.sendFile(__dirname + '/index.html');
     })
+}else{
+    app.use(express.static(__dirname + '/public'));
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/index.html');
+    })
 }
-
-// if (process.env.NODE_ENV == 'production') {
-//     app.use(express.static(path.join(__dirname, "./public")));
-
-//     app.get("/", function(_, res) {
-//         res.sendFile(
-//             path.join(__dirname, "./public/index.html"),
-//             function (err) {
-//                 if(err) {
-//                     res.status(500).send(err)
-//                 }
-//             }
-//         )
-//     })
-// } else {
-//     app.use(express.static(__dirname + '/public'));
-//     app.get('/', (req, res) => {
-//         res.sendFile(__dirname + '/index.html');
-//     })
-// }
 
 //-socket setUp in server.js-//
 const io = require('socket.io')(http);
@@ -45,3 +26,7 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('message', msg)
     });
 }); 
+
+http.listen(process.env.PORT || 3000, () => {
+    console.log("Listening on port " + (process.env.PORT || 3000));
+})
